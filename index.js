452be299126bd63449abd0d28aba50cef3177d72
Javan Oyugi <jooyugi@safaricom.co.ke>
2023-07-21 14:34:29 +0300

    session cookie name

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(cors())
 app.use(
     cookieSession({
-        name: "token",
+        name: "session-cookie",
         secureProxy: process.env.SECURE_PROXY ==="true",
         secret: process.env.PRIVATE_KEY,
         httpOnly: process.env.COOKIE_HTTP_ONLY ==="true",
